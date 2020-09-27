c4323f604d655f9aa40cb779e2654b7c6028a03b
Aniruddh <aniruddh.upparna@skan.ai>
2020-09-27 14:31:10 +0530

    intermediate commit

---
+++ gulpfile.js
@@ -0,0 +1,73 @@
+const { src, series, watch, dest } = require("gulp");
+const browserSync = require("browser-sync");
+const babel = require("gulp-babel");
+const uglify = require("gulp-uglify");
+const cleanCSS = require("gulp-clean-css");
+const autoprefixer = require("gulp-autoprefixer");
+const rename = require("gulp-rename");
+const webpack = require("webpack-stream");
+var concat = require("gulp-concat");
+var concatCss = require("gulp-concat-css");
+var htmlmin = require("gulp-htmlmin");
+var compress = require("compression");
+
+function copy() {
+  return src("src/**/*").pipe(dest("build/"));
+}
+
+function processJs() {
+  return src("src/js/*.js")
+    .pipe(
+      babel({
+        presets: ["env"]
+      })
+    )
+    .pipe(concat("index.js"))
+    .pipe(uglify())
+    .pipe(
+      webpack({
+        output: {
+          filename: "index.min.js"
+        }
+      })
+    )
+    .pipe(dest("build/js"));
+}
+
+function processCss() {
+  return src("src/css/index.css")
+    .pipe(autoprefixer())
+    .pipe(concatCss("index.css"))
+    .pipe(cleanCSS())
+    .pipe(
+      rename({
+        suffix: ".min"
+      })
+    )
+    .pipe(dest("build/css"));
+}
+
+function serve() {
+  return browserSync.init({
+    server: "build",
+    open: false,
+    port: 7000,
+    middleware: [compress()],
+    watch: true
+  });
+}
+
+function minifyHtml() {
+  return src("src/index.html")
+    .pipe(htmlmin({ collapseWhitespace: true }))
+    .pipe(dest("build"));
+}
+
+watch("src/css/*.css", processCss);
+
+watch("src/js/*.js", processJs);
+
+watch("src/*.html", minifyHtml);
+
+exports.build = series(copy, processJs, processCss, serve);
+exports.default = series(serve, watch);
---
+++ src/js/index.js
@@ -0,0 +1,26 @@
+import $ from "jquery";
+$(document).ready(main);
+
+function main() {
+  // var observer = new IntersectionObserver(callback);
+  // $(".animate-src").each(animate);
+  // function animate(index, item) {
+  //   observer.observe(item);
+  // }
+  // function callback(entries) {
+  //   entries.forEach(entry => {
+  //     if (entry.intersectionRatio > 0.7) {
+  //       entry.target.classList.add("active-transition");
+  //     }
+  //   });
+  // }
+  // $(".js-header-btn").on("click", handleNavClick);
+  // function handleNavClick(e) {
+  //   if ($(".active-button")) {
+  //     $(".active-button").removeClass("active-button");
+  //   }
+  //   var scrollto = "." + e.currentTarget.getAttribute("data-scrollto");
+  //   $(scrollto)[0].scrollIntoView({ behavior: "smooth" });
+  //   e.currentTarget.classList.add("active-button");
+  // }
+}
